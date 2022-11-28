a5490892dde6ac95885d29d7fc316c7a768dcdc1
Renan Silveira <renan.silveira01@edu.pucrs.br>
2022-11-28 15:38:33 -0300

    Add files via upload

---
+++ TrabalhoFinalPoo/EspacoNave/EspacoNave.java
@@ -0,0 +1,19 @@
+package EspacoNave;
+
+public class EspacoNave {
+    private String nome;
+
+    public EspacoNave(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+}
