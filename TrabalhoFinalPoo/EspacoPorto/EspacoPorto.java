75a83ffa0364b400bbecd5fe56edcf3c11e7a2b1
Renan Silveira <renan.silveira01@edu.pucrs.br>
2022-11-28 15:37:36 -0300

    Add files via upload

---
+++ TrabalhoFinalPoo/EspacoPorto/EspacoPorto.java
@@ -0,0 +1,19 @@
+package EspacoPorto;
+
+public class EspacoPorto{
+    int numero;
+    String nome;
+    double coordX;
+    double coordY;
+    double coordZ;
+
+    public EspacoPorto(int numero, String nome, double coordX, double coordY, double coordZ){
+        this.numero = numero;
+        this.nome = nome;
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.coordZ = coordZ;
+    }
+
+    
+}
